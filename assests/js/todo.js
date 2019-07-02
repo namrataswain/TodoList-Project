6c734d37c96c9595cb4b6eed7763e19f672fbf51
Namrata Swain <namrataswain456@gmail.com>
2019-07-02 22:20:46 +0530

    Basic template, added toggling feature to each specific clicked li

--- assests/js/todo.js
+++ assests/js/todo.js
@@ -1,1 +1,23 @@
-alert("Connected"):
+//Check off Specific Todos By clicking on it 
+$("li").click(function(){
+	// if li is gray
+	
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		console.log("it is currently gray");
+		$(this).css({
+			// turn it black
+			color: 'black',
+			textDecoration : "none"
+		});
+	}
+
+	//else
+	//turn it gray
+	else{
+		$(this).css({
+		color : "grey",
+		textDecoration :"line-through"
+		});
+	}
+	
+});
