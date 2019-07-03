9100647eb4753cdc879e1c898e71ccee01052ade
Namrata Swain <namrataswain456@gmail.com>
2019-07-03 22:07:28 +0530

    Every li with a X span is added to the existing ul

--- assests/js/todo.js
+++ assests/js/todo.js
@@ -11,6 +11,11 @@ $("span").click(function(event){
 });
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-		console.log("you entered a enter");
+		// grabbing new todo text from input
+	 var todoText = $(this).val();
+	 //after enter is hit, it resets the text input
+	 $(this).val("");
+	 // create a new li and add to ul
+	 $("ul").append("<li><span>X</span> " + todoText + "</li>");
 	}
-})
+});
