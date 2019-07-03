14895b33d39266fbb43414741a14b406e8283d28
Namrata Swain <namrataswain456@gmail.com>
2019-07-03 20:17:59 +0530

    using the toggleClass method switching between the 2 diffferent styles

--- assests/js/todo.js
+++ assests/js/todo.js
@@ -1,23 +1,4 @@
 //Check off Specific Todos By clicking on it 
 $("li").click(function(){
-	// if li is gray
-	
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		console.log("it is currently gray");
-		$(this).css({
-			// turn it black
-			color: 'black',
-			textDecoration : "none"
-		});
-	}
-
-	//else
-	//turn it gray
-	else{
-		$(this).css({
-		color : "grey",
-		textDecoration :"line-through"
-		});
-	}
-	
+	$(this).toggleClass("completed");
 });
